f1eb3c063189f347666d8072d238ccfe8d8adbd8
Justin Lau <jlau7248@gmail.com>
2017-07-10 20:58:20 -1000

    added evenNumArr function

--- exercises.js
+++ exercises.js
@@ -52,12 +52,38 @@ This function will iterate through the person parameter and console.log the foll
 "President person was a great leader."
 */
 
+var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"]
 
+for(var i=0; i<presidents.length; i++)
+{
+	console.log("The value at ", + i + " is " + presidents[i])
+}
+
+console.log(presidents.length);
+
+function leaders(person){
+	for(var i=0; i<person.length; i++)
+	{
+		console.log("President" + " " + person[i] + " " + "was a great leader");
+	}
+}
+
+leaders(presidents);
 
 /* 4) Line Number
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
+
+var stringOfNumbers = "";
+
+for(var i=10; i<21; i++)
+{
+	stringOfNumbers += i;
+
+}
+
+console.log(stringOfNumbers);
 
 
 /* 5) Even Stevens
@@ -67,6 +93,14 @@ Write a for-loop that will push even numbers to the `evenNumArr` array. We want 
 Console.log your results.
 */
 
+var evenNumArr = [];
+
+for(var i=0; i<100; i++){ 
+	if(i%2 ===0){ 
+		evenNumArr.push(i);  
+	}
+}
+console.log(evenNumArr);
 
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
